da99c16b83cd28845b5bfab1679961d455575edb
Tuomas Tolonen <Tuomas.Tolonen@gmail.com>
2021-09-12 18:22:35 +0300

    Change inheritance
    Gui now derives from sf::Drawable instead of Scene.

--- src/Gui.h
+++ src/Gui.h
@@ -2,23 +2,24 @@
 #include <list>
 #include <memory>
 #include "Widget.h"
-#include "Scene.h"
 
-class Gui : public Scene
+class Gui : public sf::Drawable
 {
 protected:
+	sf::RenderWindow& m_window;
 	sf::Font& m_font;
 public:
 	Gui(sf::RenderWindow& window, sf::Font& font)
-		: Scene(window), m_font(font) {};
+		: m_window(window), m_font(font) {};
 
 	std::list<std::shared_ptr<Widget>> m_widgets;
 
 	void addWidget(const std::shared_ptr<Widget>& widget);
 	void removeWidget(const std::shared_ptr<Widget>& widget);
 
-	// Inherited via Scene
-	virtual void handleInput(sf::Event& event, float dt) override;
-	virtual void update(float dt) override;
+	virtual void handleInput(sf::Event& event, float dt);
+	virtual void update(float dt);
+	
+	// Inherited via Drawable
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
